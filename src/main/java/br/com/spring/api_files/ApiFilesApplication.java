2ae9dc5aac0bda62b4ddf141a11bafd39a044b2e
Nivaldo-Silva <nivaldosilva.contato@gmail.com>
2024-03-01 15:16:31 -0300

    first commit

---
+++ src/main/java/br/com/spring/api_files/ApiFilesApplication.java
@@ -0,0 +1,13 @@
+package br.com.spring.api_files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFilesApplication.class, args);
+	}
+
+}
